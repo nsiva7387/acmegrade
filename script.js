5a3c128847e79be9729bc0b2a2091c5971fd9f6a
siva <nsiva7387@gmail.com>
2024-11-05 10:41:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slidebar').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2500,
+    });
+    
+  });
